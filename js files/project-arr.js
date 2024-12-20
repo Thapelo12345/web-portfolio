235184bbd19335f32516813e7ea3c713b85a4d65
Thapelo12345 <79000570+Thapelo12345@users.noreply.github.com>
2024-12-20 13:05:11 +0200

    new portfolio
    An updated version of my old portfolio

---
+++ js files/project-arr.js
@@ -0,0 +1,22 @@
+const arr = [
+    {
+        title:"React Store",
+        picUrl: "pic/project-images/multi screens/store.png",
+        link: "https://thapelo12345.github.io/store/",
+        description: "A React-based clothing store offers a seamless, interactive shopping experience with dynamic product displays, real-time updates, and smooth navigation. It features a modern design, responsive layout, and fast-loading pages. Customers can filter items, view detailed descriptions, and easily add products to their cart for a streamlined checkout process."
+    },
+    {
+        title: "Chat App",
+        picUrl: "pic/project-images/multi screens/chat.png",
+        link: "https://chat-6o8u.onrender.com/",
+        description: "The simple chat app offers a seamless communication platform, enabling private and group chats with ease. Users can send messages securely in one-on-one conversations or join group chats to connect with multiple participants. The app features a user-friendly interface, instant messaging, and notifications to stay updated. It's perfect for both casual and professional use, ensuring fast, reliable communication. Stay connected anytime, anywhere, with this versatile chat solution."
+    },
+    {
+        title: "Movie App",
+        picUrl: "pic/project-images/multi screens/movie.png",
+        link: "https://thapelo12345.github.io/movie-app/",
+        description: "In this project, I initially used free APIs available to me. However, since I am learning to write code, I thought it would be a great idea to build my own API. So, I did, and I have it hosted on Render. I built both the API and the web page you see here. For the app, I used React, React Router, and an external API to fetch movie data. The purpose of this app is to fetch movies and display information such as the movie's length, genres, and a brief description of what the movie is about."
+
+    }
+
+]
---
+++ js files/projects.js
@@ -0,0 +1,50 @@
+function navigatePage(val){
+
+let crrElement;
+
+if(val === "projects"){
+    $('#page-sliders').css('transform', 'translateX(0)')
+    crrElement = $('#project-btn')
+}//end of if
+else if(val === "about me"){
+    $('#page-sliders').css('transform', 'translateX(-100%)')
+    crrElement = $('#about-btn')
+}//end of else if first
+else if(val === "what i do"){
+    $('#page-sliders').css('transform', 'translateX(-200%)')
+    crrElement = $('#do-btn')
+}//end of if else second
+
+// console.log(`This button clicked : ${crrElement}`)
+let idArr = ['#project-btn', '#about-btn', '#do-btn']
+
+idArr.forEach((item)=>{
+
+    if($(item).parent().attr('Class') === 'animate-btns'){
+        $(item).prop('disabled', false)
+        $(item).parent().removeClass('animate-btns')
+        $(item).parent().addClass('btn-holder')
+    }//end of if
+})//end of each loop
+
+crrElement.prop('disabled', true)
+crrElement.parent().removeClass('btn-holder')
+crrElement.parent().addClass('animate-btns')
+
+}//end of navigate page
+
+function insertProject(val){
+    let projectItem = arr.find((project) => project.title === $('#project-name').text())
+    let pos = arr.indexOf(projectItem)
+
+if(val === 'next'){pos + 1 >= arr.length ? pos = 0 : ++pos}//end of if
+else{pos - 1 === -1 ? pos = arr.length - 1 : --pos}//end of else
+
+let project = arr[pos]
+
+$('#project-name').text(project.title)
+$("#project-image").attr('src', project.picUrl)
+$("#project-description").text(project.description)
+$("#project-link-holder").attr('href', project.link)
+
+}//end of insert project func
