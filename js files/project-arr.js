c62a8f2c5986bdf3494bdf9e54109717b42e5567
Thapelo12345 <79000570+Thapelo12345@users.noreply.github.com>
2024-12-21 14:49:45 +0200

    update now

--- js files/project-arr.js
+++ js files/project-arr.js
@@ -2,7 +2,7 @@ const arr = [
     {
         title:"React Store",
         picUrl: "pic/project-images/multi screens/store.png",
-        link: "https://thapelo12345.github.io/store/",
+        link: "https://thapelo12345.github.io/demo-store/",
         description: "A React-based clothing store offers a seamless, interactive shopping experience with dynamic product displays, real-time updates, and smooth navigation. It features a modern design, responsive layout, and fast-loading pages. Customers can filter items, view detailed descriptions, and easily add products to their cart for a streamlined checkout process."
     },
     {
@@ -13,7 +13,7 @@ const arr = [
     },
     {
         title: "Movie App",
-        picUrl: "pic/project-images/multi screens/movie.png",
+        picUrl: "pic/project-images/multi screens/movies.png",
         link: "https://thapelo12345.github.io/movie-app/",
         description: "In this project, I initially used free APIs available to me. However, since I am learning to write code, I thought it would be a great idea to build my own API. So, I did, and I have it hosted on Render. I built both the API and the web page you see here. For the app, I used React, React Router, and an external API to fetch movie data. The purpose of this app is to fetch movies and display information such as the movie's length, genres, and a brief description of what the movie is about."
 
